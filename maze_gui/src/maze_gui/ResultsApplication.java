1bec27b774cc1917ab430093d4baf7a6be236421
Oamogetswe Motshwanedi <oamogetswemotshwanedi68@gmail.com>
2025-05-21 03:45:05 +0200

    Explanation for user on the pane
    with the brain icon added. still working on the padding

--- maze_gui/src/maze_gui/ResultsApplication.java
+++ maze_gui/src/maze_gui/ResultsApplication.java
@@ -9,12 +9,14 @@ import GraphADT.SuperPixel;
 import GraphADT.Vertex;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -59,6 +61,10 @@ public class ResultsApplication{
 	            }
 	        });
 	}
+	// From InfoPanel
+	private void setStyleAllign(Pane pane) {
+		pane.getStyleClass().add(".vbox");
+	}
 	
 	
 	public void startWin() {
@@ -69,11 +75,31 @@ public class ResultsApplication{
 				VBox controlPane = new VBox();
 				HBox imageVBox = new HBox();
 				
-				Label explain = new Label("explanation will go here");
+				/**
+				 * The game shows this maze on the screen, and you can try to solve it. 
+				 * But the game also has a cool feature: it can analyze the maze to find parts that look similar to each other,
+				 *  like paths that have the same color or are close together. 
+				 *  This is called similarity detection, and it helps the game understand the maze better. 
+				 *  The game draws red lines between these similar parts so you can see them clearly.
+				 */
+				Label explain = new Label(
+						" 		* The game shows this maze on the screen, and you can try to solve it. \r\n"
+				+ "				* But the game also has a cool feature: it can analyze the maze to find parts that look similar to each other,\r\n"
+				+ "				* like paths that have the same color or are close together. \r\n"
+				+ "				* This is called similarity detection, and it helps the game understand the maze better. \r\n"
+				+ "				* The game draws red lines between these similar parts so you can see them clearly. :)"
+				);
+				explain.setAlignment(Pos.BASELINE_CENTER);
+				
+				
 				imageVBox.getChildren().addAll(explain,currentImage);
 				setButton();
 
 				controlPane.getChildren().addAll(imageVBox,similarityButton);
+				setStyleAllign(controlPane);
+				
+				//Icon
+				arg0.getIcons().add(new Image("/utilityImg/brain.png"));
 				
 				arg0.setScene(new Scene(controlPane));
 				arg0.show();	
