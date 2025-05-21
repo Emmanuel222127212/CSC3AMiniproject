faa1d2e95f578e6dc4a58ab905d6ec24f5414f45
Oamogetswe Motshwanedi <oamogetswemotshwanedi68@gmail.com>
2025-05-21 15:45:24 +0200

    Modified SimilarityDetector

--- maze_gui/src/GraphADT/MazeProcessor.java
+++ maze_gui/src/GraphADT/MazeProcessor.java
@@ -18,7 +18,11 @@ public class MazeProcessor {
     private BufferedImage mazeImage;
     private int width, height;
     
-    private ArrayList<SuperPixel> superPixelList;
+ //   private ArrayList<SuperPixel> superPixelList;
+    
+    private ArrayList<SuperPixel> userPX;
+    private ArrayList<SuperPixel> systemPx;
+    
     private SimilarityDetector similarityDetector;
     private ImageView imageView; // To update the displayed image
 
@@ -48,58 +52,110 @@ public class MazeProcessor {
         }
     }
 
-    private void segmentSuperpixels() {
-        superPixelList = new ArrayList<>();
+    private void segmentSuperpixels() 
+    {
+    	//changes here
+        userPX = new ArrayList<>();
+        systemPx = new ArrayList<>();
+        
         int gridSize = 5;
 
-        for (int row = 0; row < height; row += gridSize) {
-            for (int col = 0; col < width; col += gridSize) {
-                SuperPixel sp = new SuperPixel();
-                boolean hasPath = false;
-                for (int r = row; r < row + gridSize && r < height; r++) {
-                    for (int c = col; c < col + gridSize && c < width; c++) {
+        for (int row = 0; row < height; row += gridSize)
+        {
+            for (int col = 0; col < width; col += gridSize) 
+            {
+            	
+                SuperPixel userSp = new SuperPixel();
+                SuperPixel systemSp = new SuperPixel();
+                
+                //boolean hasPath = false;
+                
+                for (int r = row; r < row + gridSize && r < height; r++)
+                {
+                    for (int c = col; c < col + gridSize && c < width; c++) 
+                    {
                         int rgb = mazeImage.getRGB(c, r);
                         int gValue = (rgb >> 8) & 0xff;
-                        if (gValue > 200) { // Green channel for green image
-                            sp.AddPixel(new Pixel(c, r, gValue)); // Use green channel for intensity
-                            hasPath = true;
+                        int bValue = rgb & 0xff;
+                        
+                        if (gValue > 200) 
+                        { //System path
+                            systemSp.AddPixel(new Pixel(c, r, gValue)); // Use green channel for intensity
+                           // hasPath = true;
+                            
+                        }
+                        //user path
+                        if(bValue > 200)
+                        {
+                        	userSp.AddPixel(new Pixel(c,r,bValue));
                         }
                     }
                 }
-                if (hasPath) {
+                
+                
+                //changed here
+                if(systemSp.getAllPixels().size()>0)
+                {
+                	systemSp.CalculateCetroids();
+                	if(systemSp.GetType() == 1)
+                	{
+                		systemPx.add(systemSp);
+                	}
+                } 
+                
+                if(userSp.getAllPixels().size()>0)
+                {
+                	userSp.CalculateCetroids();
+                	if(systemSp.GetType() == 1)
+                	{
+                		userPX.add(userSp);
+                	}
+                } 
+              /*  if (hasPath)
+               *  {
                     sp.CalculateCetroids(); // Sets centroids and type
                     if (sp.GetType() == 1) { // Only add path superpixels (type 1)
                         superPixelList.add(sp);
                     }
-                }
+                }*/
             }
         }
-        similarityDetector = new SimilarityDetector(superPixelList, mazeImage);
-        System.out.println("Generated " + superPixelList.size() + " superpixels");
+        similarityDetector = new SimilarityDetector(userPX,systemPx, mazeImage);
+        System.out.println("Generated " + userPX.size() + " user pixels and " + systemPx.size() + " System superPixels");
     }
 
-    private void computeSimilaritiesAndVisualize() {
+    private void computeSimilaritiesAndVisualize() 
+    {
         BufferedImage visualization = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics2D g2d = visualization.createGraphics();
+        
         g2d.drawImage(mazeImage, 0, 0, null);
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setColor(Color.RED);
         g2d.setStroke(new BasicStroke(1));
 
         int similarPairs = 0;
-        for (int id1 = 0; id1 < superPixelList.size(); id1++) {
-            for (int id2 = id1 + 1; id2 < superPixelList.size(); id2++) {
-                double similarity = similarityDetector.computeSimilarity(id1, id2);
-                if (similarity > 0.95) {
+        
+        for (int UID = 0; UID < userPX.size(); UID++) 
+       {
+            for (int SID = 0; SID < systemPx.size(); SID++) 
+            
+            {
+                double similarity = similarityDetector.computeSimilarity(UID, SID);
+                
+                if (similarity > 0.80) 
+                {
                     similarPairs++;
-                    SuperPixel sp1 = superPixelList.get(id1);
-                    SuperPixel sp2 = superPixelList.get(id2);
+                    SuperPixel sp1 = userPX.get(UID);
+                    SuperPixel sp2 = systemPx.get(SID);
+                    
                     int x1 = sp1.getAvgPixelXPos();
                     int y1 = sp1.getyAvgPixelYPos();
                     int x2 = sp2.getAvgPixelXPos();
                     int y2 = sp2.getyAvgPixelYPos();
+                    
                     g2d.drawLine(x1, y1, x2, y2);
-                    System.out.println("Similar pair " + id1 + " and " + id2 + " (Similarity: " + String.format("%.3f", similarity) + ")");
+                    System.out.println("Similar pair(User) " + UID + " and " + SID + " (Similarity: " + String.format("%.3f", similarity) + ")");
                 }
             }
         }
@@ -124,4 +180,28 @@ public class MazeProcessor {
         Image fxImage = SwingFXUtils.toFXImage(mazeImage, null);
         imageView.setImage(fxImage);
     }
+
+  //getters for the new user and System Super pixel lists
+	public ArrayList<SuperPixel> getUserPX() 
+	{
+		return userPX;
+	}
+
+	public ArrayList<SuperPixel> getSystemPx() 
+	{
+		return systemPx;
+	}
+
+	
+	public BufferedImage getBufferedImage() 
+	{
+		
+		return mazeImage;
+	}
+    
+    
+    
+    
+    
+    
 }
--- maze_gui/src/maze_gui/ResultsApplication.java
+++ maze_gui/src/maze_gui/ResultsApplication.java
@@ -2,15 +2,20 @@ package maze_gui;
 
 
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 
+import GraphADT.ArrayList;
 import GraphADT.MazeProcessor;
+import GraphADT.SimilarityDetector;
 import GraphADT.SuperPixel;
 import GraphADT.Vertex;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -28,26 +33,58 @@ public class ResultsApplication{
 	private File currentFile;
 	Button similarityButton = new Button("Show Similarity");
 	
-	public ResultsApplication(Image I) {
+	//to show the similarity
+	Canvas overlaySimilarity ;
+	Pane imagePane ;
+	
+	public ResultsApplication(Image I)
+	{
 		
-		        currentImage = new ImageView(I);
-		
-		        System.out.println("Error: Image is null!");
-		  
-
-		mazeProcessor =  new MazeProcessor(currentImage);
+        currentImage = new ImageView(I);
+        System.out.println("Error: Image is null!");
+        mazeProcessor =  new MazeProcessor(currentImage);
+        
+        if (currentImage != null && currentImage.getImage() != null) 
+        {
+            overlaySimilarity = new Canvas(currentImage.getImage().getWidth(), currentImage.getImage().getHeight());
+        } 
+        else
+        {
+            System.out.println("Image is not loaded yet, cannot create canvas.");
+        }
+        
+        imagePane = new Pane(currentImage,overlaySimilarity);
 	}
 	
-	public ResultsApplication(File i) {
+	public ResultsApplication(File i) 
+	{
 		currentFile = i;
 		currentImage = new ImageView(new Image(currentFile.toURI().toString()));
+		
 		mazeProcessor =  new MazeProcessor(currentImage);
+		
+		if (currentImage != null && currentImage.getImage() != null) 
+        {
+            overlaySimilarity = new Canvas(currentImage.getImage().getWidth(), currentImage.getImage().getHeight());
+        } 
+        else
+        {
+            System.out.println("Image is not loaded yet, cannot create canvas.");
+        }
+		
+		imagePane = new Pane(currentImage,overlaySimilarity);
 	}
 	
 
 	public ResultsApplication(GraphADT.ArrayList<Vertex<SuperPixel>> playerpath, Image I) {
-		 new ResultsApplication(I);
+		// new ResultsApplication(I);
 		//handle logic for drawing the path
+		this(I);
+		if(I == null)
+		{
+			throw new IllegalArgumentException("Image cant be null");
+		}
+		
 	}
 	
 	private void setButton() {
@@ -58,7 +95,22 @@ public class ResultsApplication{
 	            if (currentFile != null) {
 	            	System.out.println(currentFile.getPath());
 	                mazeProcessor.processMazeImage(currentFile.getPath().toString());
-	            } else {
+	                
+	                
+	                //
+	                // Assume you have user and system superpixels from MazeProcessor
+	                ArrayList<SuperPixel> userSPs = mazeProcessor.getUserPX();
+	                ArrayList<SuperPixel> systemSPs = mazeProcessor.getSystemPx();
+	                BufferedImage bufferedImage = mazeProcessor.getBufferedImage();
+
+	                SimilarityDetector detector = new SimilarityDetector(userSPs, systemSPs, bufferedImage);
+	                ArrayList<SuperPixel[]> matchedPairs = detector.findMostSimilarPairs(0.8);
+	                // 0.8 similarity threshold
+	                
+	                drawSimilarityLines(overlaySimilarity,matchedPairs);
+	            }
+	            else 
+	            {
 	                System.out.println("No image selected.");
 	            }
 	        });
@@ -86,11 +138,25 @@ public class ResultsApplication{
 				 */
 				
 				String expText =  
-						" The game shows this maze on the screen, and you can try to solve it. \r\n"
-								+"But the game also has a cool feature: it can analyze the maze to find parts that look similar to each other\r\n"
-								+"like paths that have the same color or are close together. \r\n"
-								+"This is called similarity detection, and it helps the game understand the maze better. \r\n"
-								+"The game draws red lines between these similar parts so you can see them clearly. :) ";
+						"  The maze is divided into tiny blocks, called superpixels. These are small regions that represent parts of the maze.\r\n"
+						+ "\r\n"
+						+ "The app looks for two different paths in the image:\r\n"
+						+ "\r\n"
+						+ "*The solution path (usually shown in green).\r\n"
+						+ "\r\n"
+						+ "*The user's path (usually shown in blue — the one you drew).\r\n"
+						+ "\r\n"
+						+ "For each part of the user’s path, the app tries to find a similar part of the solution path, based on two things:\r\n"
+						+ "\r\n"
+						+ "-Color similarity (are they similar shades?).\r\n"
+						+ "\r\n"
+						+ "-Position similarity (are they close together in the maze).\r\n"
+						+ "\r\n"
+						+ "*When the app finds a close match between a user and solution superpixel, it:\r\n"
+						+ "\r\n"
+						+ "Calculates a similarity score (from 0 to 1).\r\n"
+						+ "\r\n"
+						+ "If the score is high enough, it draws a red line connecting the matching parts — so you can see where your path matches the correct one. ";
 				Label explain = new Label(expText);
 				
 				explain.setWrapText(true);
@@ -101,7 +167,8 @@ public class ResultsApplication{
 				explanation.setMaxWidth(500);
 				
 				
-				imageVBox.getChildren().addAll(explanation,currentImage);
+				//imageVBox.getChildren().addAll(explanation,currentImage);
+				imageVBox.getChildren().addAll(explanation,imagePane);
 				setButton();
 
 				controlPane.getChildren().addAll(imageVBox,similarityButton);
@@ -111,8 +178,29 @@ public class ResultsApplication{
 				arg0.getIcons().add(new Image("/utilityImg/brain.png"));
 				
 				arg0.setScene(new Scene(controlPane));
+				arg0.setTitle("Similarity Detection");
 				arg0.show();	
 		});
+	}
+	
+	
+	private void drawSimilarityLines(Canvas canvas, GraphADT.ArrayList<SuperPixel[]> matchedPairs)
+	{
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		
+		for(SuperPixel[] pairs : matchedPairs)
+		{
+			SuperPixel userPx = pairs[0];
+			SuperPixel systemPx = pairs[1];
+			
+			
+			double x1 = userPx.getAvgPixelXPos();
+	        double y1 = userPx.getyAvgPixelYPos();
+	        double x2 = systemPx.getAvgPixelXPos();
+	        double y2 = systemPx.getyAvgPixelYPos();
+
+	        gc.strokeLine(x1, y1, x2, y2);
+		}
 	}
 
 }
